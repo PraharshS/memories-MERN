084d0ca849f2cad9e4c0018073828876a6334608
Praharsh Singh <praharsh535@gmail.com>
2022-04-06 23:03:20 +0530

    basic actions and client side pages

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import postRoutes from "./routes/posts.js";
 const app = express();
+
+app.use(cors());
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 
 const CONNECTION_URL = `mongodb+srv://zitrakz:mongopass747@pdgcluster.txsie.mongodb.net/memories-MERN?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
